0a3432fdacd37be451a4e3bd9c02ff3ed8bc9621
noma <harinezumilove1106@gmail.com>
2018-05-24 20:57:03 +0900

    commit

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -16,22 +16,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
+  get static_pages_help_url
+  assert_response :success
+  assert_select "title", "Ruby on Rails Tutorial Sample App"
+end
 
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
+test "should get about" do
+  get static_pages_about_url
+  assert_response :success
+  assert_select "title", "Ruby on Rails Tutorial Sample App"
+end
 
-
-  test "should get contact" do
-   get static_pages_contact_url
-   assert_response :success
-   assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
-
+test "should get contact" do
+  get static_pages_contact_url
+  assert_response :success
+  assert_select "title", "Ruby on Rails Tutorial Sample App"
+end
 end
