d3381c53be9fd860abf4184e989e58450abe8c6d
noma <harinezumilove1106@gmail.com>
2018-05-25 15:37:17 +0900

    コミット！

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
-#リスト 6.5: 有効なUserかどうかをテストする
+
 class UserTest < ActiveSupport::TestCase
+  #リスト 6.5: 有効なUserかどうかをテストする
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
@@ -8,7 +9,7 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-
+  #name属性にバリデーションに対するテスト
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
@@ -18,5 +19,39 @@ class UserTest < ActiveSupport::TestCase
      @user.email = "     "
      assert_not @user.valid?
    end
+   #リスト 6.14: nameの長さの検証に対するテスト
+   test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  #リスト 6.18: 有効なメールフォーマットをテストする
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  #メールフォーマットの検証に対するテスト
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  #リスト 6.24: 重複するメールアドレス拒否のテスト
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
 end
