fd72c230ed75eaa4141228402027758ba03607c6
noma <harinezumilove1106@gmail.com>
2018-05-27 13:56:59 +0900

    6.26: 大文字小文字を区別しない、一意性のテスト

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -50,6 +50,7 @@ class UserTest < ActiveSupport::TestCase
   #リスト 6.24: 重複するメールアドレス拒否のテスト
   test "email addresses should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
