aaac93881b928a4e34a30f6d01096089b7d9c02f
noma <harinezumilove1106@gmail.com>
2018-05-27 20:52:46 +0900

    7.5:

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+
+  def show
+  @user = User.find(params[:id])
+  end
+
   def new
   end
+
 end
+
+
+今日の反省
+
+集中力が足りなくて、自分のコードとrailstutorialのコーどを見比べられていなかった
+
+理解できるようにしましょう！
+進捗は一言で表す！
