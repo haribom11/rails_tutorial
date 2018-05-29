70afece3820cf284d180f3be56675cc4e271b284
noma <harinezumilove1106@gmail.com>
2018-05-29 10:04:06 +0900

    リスト 8.11: ログイン失敗時の正しい処理 green destroy追加

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -4,11 +4,14 @@ class SessionsController < ApplicationController
 
   def create
    user = User.find_by(email: params[:session][:email].downcase)
-   if user && user.authenticate(params[:session][:password])
-     # ユーザーログイン後にユーザー情報のページにリダイレクトする
-   else
-      flash.now[:danger] = 'Invalid email/password combination'
-     render 'new'
+     if user && user.authenticate(params[:session][:password])
+       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+     else
+        flash.now[:danger] = 'Invalid email/password combination'
+       render 'new'
+     end
    end
- end
+
+   def destroy
+   end
 end
