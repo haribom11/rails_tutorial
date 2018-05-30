0c4e664855d87c3594b165689cfd9eec7f562b07
noma <harinezumilove1106@gmail.com>
2018-05-30 16:09:51 +0900

    エラーのない状態に

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -2,20 +2,13 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  def setup
-  @user = users(:michael)
-  end
-  #上のusersはfixtureのファイル名users.ymlを表し、:michaelというシンボルはリスト 8.22のユーザーを参照するためのキーを表します。
-
   test "login with invalid information" do
     get login_path
-    post login_path, params: { session: { email: "", password: "password" } }
-    assert_redirected_to @user
-    #リダイレクト先が正しいかどうかをチェック
-    follow_redirect! #ログインパスのリンクがページにないかどうかで判定
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0 #渡したパターンに一致するリンクが０かどうかを確認
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
