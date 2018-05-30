eee6a9ddc3421de5cc4b1faef5639c0124cda6c5
noma <harinezumilove1106@gmail.com>
2018-05-30 15:53:24 +0900

    コメント

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -10,10 +10,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     post login_path, params: { session: { email: "", password: "password" } }
-    assert_redirected_to @user
-    follow_redirect!
+    assert_redirected_to @user #リダイレクト先が正しいかどうかをチェック
+    follow_redirect! #ログインパスのリンクがページにないかどうかで判定
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", login_path, count: 0 #渡したパターンに一致するリンクが０かどうかを確認
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
