55a9e8f9dc506c707390958c09837a0a27d6f4bd
noma <harinezumilove1106@gmail.com>
2018-06-02 12:36:42 +0900

    #リスト 9.31: 永続的セッションのテスト

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -29,4 +29,18 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      raise  # テストがパスすれば、この部分がテストされていないことがわかる
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 end
