f907195013eb41998a20be87a9cf01f2dc1cf6fc
noma <harinezumilove1106@gmail.com>
2018-06-02 14:50:02 +0900

    リスト 9.33: 例外発生部分を削除する

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,6 +3,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -35,7 +43,8 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise  # テストがパスすれば、この部分がテストされていないことがわかる
+    
+
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -43,4 +52,5 @@ module SessionsHelper
       end
     end
   end
+
 end
