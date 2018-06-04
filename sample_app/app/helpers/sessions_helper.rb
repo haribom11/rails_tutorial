3410f910928ab189e416e05a7b8568bad7b86134
noma <harinezumilove1106@gmail.com>
2018-06-04 23:17:51 +0900

    リスト 10.27: current_user?メソッド

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+    # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -43,7 +48,6 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
