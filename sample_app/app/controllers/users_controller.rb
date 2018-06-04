7125fe5d7f083f1fd63d9bd745aec2142c736c7d
noma <harinezumilove1106@gmail.com>
2018-06-04 22:59:57 +0900

    リスト 10.25: beforeフィルターを使って編集/更新ページを保護する green

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -49,4 +49,10 @@ class UsersController < ApplicationController
     end
   end
 
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
 end
