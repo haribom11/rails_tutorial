c522334dbf8e95e2dd7a675cc20345ff0be960eb
noma <harinezumilove1106@gmail.com>
2018-06-08 20:23:43 +0900

    fix

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -23,6 +23,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              FILL_IN,
+                                            password_confirmation: FILL_IN,
+                                            admin: FILL_IN } }
+    assert_not @other_user.FILL_IN.admin?
   end
 
   test "should get new" do
