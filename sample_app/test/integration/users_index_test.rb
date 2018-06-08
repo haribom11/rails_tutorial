584ff10436baccf056d03e24c76c53316096ddd9
noma <harinezumilove1106@gmail.com>
2018-06-08 09:44:31 +0900

    リスト 10.48: ページネーションを含めたUsersIndexのテスト

--- sample_app/test/integration/users_index_test.rb
+++ sample_app/test/integration/users_index_test.rb
@@ -12,7 +12,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
-      <%= render user %>
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
